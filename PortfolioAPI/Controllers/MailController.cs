13ac334e504589a246735136806a08f601282984
TahjidEshan <atahjid@gmail.com>
2019-02-24 20:30:57 +0600

    smtp mail sending done

--- PortfolioAPI/Controllers/MailController.cs
+++ PortfolioAPI/Controllers/MailController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using portfolio.Models;
@@ -11,36 +13,65 @@ namespace portfolio.Controllers
     [ApiController]
     public class MailController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
+        // POST api/Mail
         [HttpPost]
-        public void Post(MailModel model)
+        public void Post([FromBody]MailModel model)
         {
-        }
+            SmtpClient smtp = new SmtpClient();
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("tahjideshan@gmail.com", "eshan123456.");  // [4] Added this. Note, first parameter is NOT string.
+            smtp.Host = "smtp.gmail.com";
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+            MailMessage mail = new MailMessage();
+            mail.From = new System.Net.Mail.MailAddress("tahjideshan@gmail.com");
+            mail.Priority = MailPriority.High;
+            mail.Subject = "Mail From Portfolio Site";
+            mail.ReplyToList.Add(new MailAddress("atahjid@gmail.com"));
+            mail.To.Add(new MailAddress("atahjid@gmail.com"));
+            mail.IsBodyHtml = true;
+            mail.Body = $@"
+                <html>
+                <body>
+                    <div style='padding:10px;background:#fff;'>
+                    Someone sent a message from the portfolio site at {DateTime.UtcNow}
+                        <p>
+                            Name : {model.Name},
+                            Email Address : {model.Email},
+                            <br>
+                            Phone : {model.Phone},
+                        </p>
+                        <p>
+                            {model.Message}
+                        </p>
+                    </div>
+                </body>
+            </html>";
+            smtp.Send(mail);
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            MailMessage mailToUser = new MailMessage();
+            mailToUser.From = new System.Net.Mail.MailAddress("tahjideshan@gmail.com");
+            mailToUser.Priority = MailPriority.High;
+            mailToUser.Subject = "Request recieved";
+            mailToUser.ReplyToList.Add(new MailAddress("atahjid@gmail.com"));
+            mailToUser.To.Add(new MailAddress(model.Email));
+            mailToUser.IsBodyHtml = true;
+            mailToUser.Body = $@"
+                <html>
+                <body>
+                    <div style='padding:10px;background:#fff;'>
+                    Dear {model.Name}, your message was recieved at {DateTime.UtcNow}
+                    I will get back to you ASAP.
+
+                    Thank you
+                    Tahjid
+                    </div>
+                </body>
+            </html>";
+            smtp.Send(mailToUser);
+
         }
     }
 }
